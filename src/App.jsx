43fa4f73d407b01e671f4c1d869212a4008462ac
ismael200code <ismael.200code@gmail.com>
2026-05-01 20:30:07 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold m-23 underline">Hello world !</h1>
+      <p className="text-center">Debut du projet Aksi: <i>Platefome de location de véhicule en tout genre.</i></p>
+    </>
+  )
+}
+
+export default App
